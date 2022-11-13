2e96f41ff6d4d71940b0b6ba504afe5995a9d093
anshm42 <69057488+anshm42@users.noreply.github.com>
2022-11-13 13:32:56 -0500

    lmfao

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+killall polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar ansh 2>&1 | tee -a /tmp/polybar.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Polybar launched..."
+# Launch polybar
+polybar right &
+polybar left &
+
